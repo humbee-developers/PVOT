6e14e581d1c3fa5f855c9f02de036aa46c09b6ca
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2023-08-24 18:57:24 +0530

    contact us animation

--- src/components/contactUs/ContactUs.jsx
+++ src/components/contactUs/ContactUs.jsx
@@ -1,4 +1,23 @@
+import contactImage from "../../images/contact-us.svg";
+import "./contactUs.css";
+
 const ContactUs = () => {
-  return <h1>Contact Us</h1>;
+  return (
+    <div>
+      <div className="contact-image">
+        <img src={contactImage} className="App-logo" alt="logo" />
+      </div>
+      <div className="marquee">
+        <div className="marquee--inner">
+          <span>
+            <h2>
+              Contact Us - Contact Us - Contact Us - Contact Us - Contact Us -
+              Contact Us - Contact Us - Contact Us - Contact Us - Contact Us
+            </h2>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default ContactUs;
