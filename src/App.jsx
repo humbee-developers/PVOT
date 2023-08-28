c73d82ee9c4744a522e7ebd6f5699022e6d3ddd8
utkarshyadav11 <utkarshyadav6745@gmail.com>
2023-08-28 18:04:35 +0530

    portfolio added -u

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Header from './components/header/Header';
 import HeroSection from './components/heroSection/HeroSection';
 import ContactUs from './components/contactUs/ContactUs';
 import Footer from './components/footer/Footer';
+import Portfolio from './components/portfolio/Portfolio';
 const App = () => {
   return (
     <div >
 <Header/>
    <HeroSection/>
+   <Portfolio/>
    <ContactUs/>
    <Footer/>
   
