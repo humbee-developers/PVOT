2fee6745b249525db9b29cfae81c6115523009e6
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2023-08-29 19:00:34 +0530

    Amazing 2023 ball animation

--- src/components/amazing2023/Amazing2023.jsx
+++ src/components/amazing2023/Amazing2023.jsx
@@ -1,20 +1,45 @@
 import { motion, useScroll } from "framer-motion";
 import "./amazing2023.css";
+import amazing2023Image from "../../images/amazing_2023_image.svg";
+import ball from "../../images/3d_ball.svg";
 
 const Amazing2023 = () => {
   const { scrollYProgress } = useScroll();
   return (
     <div className="amazing">
-      <div className="amazing_heading">
-        <h2>For an amazing</h2>
+      <div className="amazing_wrapper">
+        <div className="amazing_heading">
+          <h2>For an amazing</h2>
+        </div>
+        <div className="amazing_year">
+          <p>
+            <span>2</span>
+            <div className="ball_container">
+              <img src={amazing2023Image} alt="amazing_2023_image" />
+              <img src={ball} alt="3d_ball" className="ball_image" />
+            </div>
+            <span>2</span>
+            <span>3</span>
+          </p>
+        </div>
+        <div className="amazing_bgHeading">
+          <motion.h2
+            className="service-text"
+            initial={{ y: 1000 }}
+            animate={{ y: 20 }}
+            transition={{
+              duration: 1,
+              delay: 1,
+            }}
+          >
+            Amazing
+          </motion.h2>
+        </div>
+        <motion.div
+          className="progress-bar"
+          style={{ scaleX: scrollYProgress }}
+        />
       </div>
-      <motion.div
-        className="progress-bar"
-        style={{ scaleX: scrollYProgress }}
-      />
-      <h1>
-        <code>useScroll</code> demo
-      </h1>
     </div>
   );
 };
