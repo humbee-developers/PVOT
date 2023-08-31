62424cc6598dae7222d938e586ce59330f28f0db
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2023-08-31 17:09:33 +0530

    router

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,18 @@ import CareerPage from "./pages/Desktop5/CareersPage";
 import MediaPage from "./pages/Desktop7/MediaPage";
 const App = () => {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact-us" element={<ContactUsPage />} />
-        <Route path="/services" element={<ServicePage />} />
-        <Route path="/portfolio" element={<PortfolioPage />} />
-        <Route path="/career" element={<CareerPage />} />
-        <Route path="/blog" element={<MediaPage />} />
-      </Routes>
-    </Layout>
+    <>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact-us" element={<ContactUsPage />} />
+          <Route path="/services" element={<ServicePage />} />
+          <Route path="/portfolio" element={<PortfolioPage />} />
+          <Route path="/career" element={<CareerPage />} />
+          <Route path="/blog" element={<MediaPage />} />
+        </Routes>
+      </Layout>
+    </>
   );
 };
 
