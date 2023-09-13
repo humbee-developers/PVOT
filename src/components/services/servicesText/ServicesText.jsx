4f538832a0cb2c5b10d6e3624e2da0a13b13bdf5
utkarshyadav11 <utkarshyadav6745@gmail.com>
2023-09-13 12:58:05 +0530

    small fix in services-text-u

--- src/components/services/servicesText/ServicesText.jsx
+++ src/components/services/servicesText/ServicesText.jsx
@@ -18,28 +18,28 @@ const ServicesText = () => {
   useEffect(() => {
     if (inView) {
       controlsLeftToRight.start({
-        x: 1200, // Start from the left
+        x: 1400, // Start from the left
         opacity: 1,
         transition: {
-          x: { type: "spring", stiffness: 4 },
-          opacity: { duration: 2 },
+          x: { type: "spring", stiffness: 3 },
+          opacity: { duration: 1 },
         },
       });
 
       controlsRightToLeft.start({
-        x: -1400, // Start from the right
+        x: -1500, // Start from the right
         opacity: 1,
         transition: {
-          x: { type: "spring", stiffness: 4 },
-          opacity: { duration: 2 },
+          x: { type: "spring", stiffness: 3 },
+          opacity: { duration: 1 },
         },
       });
     } else {
       controlsLeftToRight.start({
-        x: -1100, // Animate back to the left when out of view
+        x: -1200, // Animate back to the left when out of view
         opacity: 1,
         transition: {
-          x: { type: "spring", stiffness: 4 },
+          x: { type: "spring", stiffness: 3 },
           opacity: { duration: 1 },
         },
       });
@@ -48,7 +48,7 @@ const ServicesText = () => {
         x: 1100, // Animate back to the right when out of view
         opacity: 1,
         transition: {
-          x: { type: "spring", stiffness: 4 },
+          x: { type: "spring", stiffness: 3 },
           opacity: { duration: 1 },
         },
       });
@@ -77,7 +77,7 @@ const ServicesText = () => {
 
         {/* Duplicate the motion.div with the same animation properties */}
         <motion.div
-          className="roller-text services_text xyz"
+          className="roller-text services_text"
           initial={{ x: 1400, opacity: 1 }} // Start position from right
           animate={controlsRightToLeft}
           // style={{ transition: "all 0.3s ease-in-out" }}
@@ -90,10 +90,10 @@ const ServicesText = () => {
           animate={controlsLeftToRight}
           // style={{ transition: "all 0.3s ease-in-out" }}
         >
-          Search Engine Optimization[seo]
+          Search Engine Optimization
         </motion.div>
         <motion.div
-          className="roller-text services_text xyz"
+          className="roller-text services_text "
           initial={{ x: 1400, opacity: 1 }} // Start position from right
           animate={controlsRightToLeft}
           // style={{ transition: "all 0.3s ease-in-out" }}
