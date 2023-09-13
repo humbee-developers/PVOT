f0435661b5f4cbcb4477a70eeb0b8f61bbaa2771
utkarshyadav11 <utkarshyadav6745@gmail.com>
2023-09-13 10:51:55 +0530

    u-u

--- src/pages/desktop8/KnowUsPage.jsx
+++ src/pages/desktop8/KnowUsPage.jsx
@@ -1,34 +1,43 @@
-import React from 'react'
 import HeroHeading from "../Desktop6/HeroHeading/HeroHeading";
 import RectangleCard from "../../../Common/RectangleCard";
-import rectangleBg from "../../images/rectangle-image.png"
-import Achievements from '../../components/achievements/Achievements';
-import "./Knowus.css"
+import rectangleBg from "../../images/rectangle-image.png";
+import Achievements from "../../components/achievements/Achievements";
+import "./Knowus.css";
+import ClientOuter from "../../components/clientOuter/ClientOuter";
+import CoreTeam from "../../components/coreTeam/CoreTeam";
 const KnowUsPage = () => {
   return (
     <div style={{ backgroundColor: "black" }}>
-    <HeroHeading  bigHeading={"KNOW US"}/>
+      <HeroHeading bigHeading={"KNOW US"} />
 
-    <div className='know-us-divider'>
-    <div className='know-us-title'>WHAT DO WE DO?</div>
-    <div className='know-us-text'>
-    <div className='know-us-text-inner'>PVOT  is a full-service creative agency that collaborates with game changers to amplify their stories and impact through thoughtful and strategic design. From advancing human rights to creating more sustainable cities and everything in between, we work to mobilize people around causes that spur positive change.</div>
+      <div className="know-us-divider">
+        <div className="know-us-title">WHAT DO WE DO?</div>
+        <div className="know-us-text">
+          <div className="know-us-text-inner">
+            PVOT is a full-service creative agency that collaborates with game
+            changers to amplify their stories and impact through thoughtful and
+            strategic design. From advancing human rights to creating more
+            sustainable cities and everything in between, we work to mobilize
+            people around causes that spur positive change.
+          </div>
+        </div>
+      </div>
+      <div className="know-us-image-outer" style={{ marginTop: "19px" }}>
+        <img
+          className="know-us-image"
+          src={rectangleBg}
+          alt="rectangle-bg"
+          style={{ width: "100%" }}
+        />
+      </div>
+      <div style={{ backgroundColor: "white" }}>
+        <Achievements />
+      </div>
+      <ClientOuter />
+      <CoreTeam />
+      <RectangleCard />
     </div>
-    </div>
-    <div className='know-us-image-outer' style={{marginTop:"19px"}}>
-    <img className='know-us-image' src={rectangleBg} alt="rectangle-bg" style={{width:"100%"}} />
-    </div>
-    <div style={{backgroundColor:"white"}}>
-    <Achievements/>
-    </div>
-  <div className='clients-outer'>
-  <div className='clients-title'>OUR CLIENTS</div>
-  <div className='clients-text'>We’re fortunate to have worked with some amazing clients.</div>
-  <img src='' alt="" />
-  </div>
-    <RectangleCard />
-    </div>
-  )
-}
+  );
+};
 
-export default KnowUsPage
+export default KnowUsPage;
