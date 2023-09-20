43942d8d96334b993a12b7d480ae5a41e8cba2e9
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2023-09-20 12:17:16 +0530

    minor changes

--- src/components/bgTransition/BgTransition.jsx
+++ src/components/bgTransition/BgTransition.jsx
@@ -7,34 +7,34 @@ const BgTransition = () => {
   const { scrollYProgress } = useScroll({
     target: ref,
   });
-  const y = useTransform(scrollYProgress, [1, 1], ["100%", "0%"]);
+  const y = useTransform(scrollYProgress, [1, 1], ["50%", "0"]);
   //   const x = useTransform(scrollYProgress, [1, 1], ["100%", "-20%"]);
   return (
     <div className="bgTransition" ref={ref}>
       <div className="bgTransition_wrapper">
         <motion.div
           className="bgTransition_1"
-          style={{ scaleY: scrollYProgress, y }}
+          style={{ scaleY: scrollYProgress, translateY:y }}
         >
-          1
+          {/* 1 */}
         </motion.div>
         <motion.div
           className="bgTransition_2"
           style={{ scaleY: scrollYProgress, y }}
         >
-          2
+          {/* 2 */}
         </motion.div>
         <motion.div
           className="bgTransition_3"
           style={{ scaleY: scrollYProgress, y }}
         >
-          3
+          {/* 3 */}
         </motion.div>
         <motion.div
           className="bgTransition_4"
           style={{ scaleY: scrollYProgress, y }}
         >
-          4
+          {/* 4 */}
         </motion.div>
       </div>
     </div>
