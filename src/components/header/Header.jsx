af52524c0e41b3af8abc2ea5138d68e4f6b43a8a
utkarshyadav11 <utkarshyadav6745@gmail.com>
2023-09-20 18:08:42 +0530

    magnetic -effect added -u

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,6 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./header.css";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import FramerMagnetic from "../../../Common/magnetic";
 const Header = () => {
   const [active, setActive] = useState(false);
 
@@ -46,38 +47,65 @@ const Header = () => {
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="" style={{ marginLeft: "auto" }}>
                 <div className="desktop_menu">
+                
                   <Link
                     to="/"
                     className="nav-link nav_icon_link"
                     style={{ backgroundColor: "white" }}
                   >
+                  <FramerMagnetic>
                     <img src={logo} alt="logo" />
+                    </FramerMagnetic>
                   </Link>
+                  
+
+                 
                   <Link to="/services" className="nav-link hover_btn">
+                  <FramerMagnetic>
+                   
                     <span className="btn_text">Services</span>
-                    <span className="btn_text">Services</span>
+                    </FramerMagnetic>
                   </Link>
+                 
+                 
                   <Link to="/knowUs" className="nav-link hover_btn">
+                  <FramerMagnetic>
                     <span className="btn_text">Know Us</span>
-                    <span className="btn_text">Know Us</span>
+                    </FramerMagnetic>
                   </Link>
+                  
+                  
                   <Link to="/portfolio" className="nav-link hover_btn">
+                  <FramerMagnetic>
                     <span className="btn_text">Portfolio</span>
-                    <span className="btn_text">Portfolio</span>
+                    </FramerMagnetic>
                   </Link>
+                  
+
+                  
                   <Link to="/blog" className="nav-link hover_btn">
+                  <FramerMagnetic>
                     <span className="btn_text">Media</span>
-                    <span className="btn_text">Media</span>
+                    </FramerMagnetic>
                   </Link>
+                 
+
+                
                   <Link to="/career" className="nav-link hover_btn">
+                  <FramerMagnetic>
                     <span className="btn_text">Career</span>
-                    <span className="btn_text">Career</span>
+                    </FramerMagnetic>
                   </Link>
+                 
+                  
                   <Link to="/contact-us" className="nav-link hover_btn">
+                  <FramerMagnetic>
                     <span className="btn_text"> Contact Us</span>
-                    <span className="btn_text"> Contact Us</span>
+                    </FramerMagnetic>
                   </Link>
+                 
                 </div>
+
                 <div className="mobile_menu">
                   <Navbar.Toggle onClick={handleToggle}>
                     <Link to="/" className="nav-link hover_btn">
