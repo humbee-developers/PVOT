7bec2ded77b970717cae771c3968d0a806a6131d
utkarshyadav11 <utkarshyadav6745@gmail.com>
2023-09-28 11:19:33 +0530

    ..

--- src/components/roler/Rooler.jsx
+++ src/components/roler/Rooler.jsx
@@ -41,7 +41,7 @@ const TextRevealEffect = () => {
       });
 
       controlsRightToLeft.start({
-        x: 1100, // Animate back to the right 
+        x: 1100, 
         opacity: 1,
         transition: {
           x: { type: "spring", stiffness: 4 },
