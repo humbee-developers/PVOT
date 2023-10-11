d5d80d3487bf435ae1040bf52e3b7d207ad6da40
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2023-10-11 16:37:07 +0530

    inner page image slider component --m

---
+++ src/components/leftImageRightSliderImage/LeftImageRightSliderImage.jsx
@@ -0,0 +1,27 @@
+// import LeftImage from "./LeftImage";
+// import RightSlider from "./RightSlider";
+// import RightSliderImage from "./RightSliderImage";
+// // import leftImage from "../../images/inner-image1-2.png";
+
+// const LeftImageRightSliderImage = () => {
+//   return (
+//     <div className="leftImageRightSliderImage">
+//       <div className="leftImageRightSliderImage_container">
+//         <div className="leftImageRightSliderImage_wrapper">
+//           <LeftImage />
+//           <div className="leftImageRightSliderImage_right">
+//             <div className="leftImageRightSliderImage_right_top">
+//               <RightSlider  />
+//               <RightSliderImage />
+//             </div>
+//             <div className="leftImageRightSliderImage_right_bottom">
+//               <RightSliderImage />
+//               <RightSlider />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+// export default LeftImageRightSliderImage
--- src/pages/Desktop3/InnerPages/BrandingAdvertising.jsx
+++ src/pages/Desktop3/InnerPages/BrandingAdvertising.jsx
@@ -1,10 +1,57 @@
-import React from "react";
 import RelatedServices from "../../../components/relatedServices/RelatedServices";
+import leftImage from "../../../images/inner-image1-2.png";
+import LeftImage from "../../../components/leftImageRightSliderImage/LeftImage";
+import RightSlider from "../../../components/leftImageRightSliderImage/RightSlider";
+import rightTopImage from "../../../images/inner-image1-4.png";
+import rightBottomImage from "../../../images/inner-image1-5.png";
+import RightSliderImage from "../../../components/leftImageRightSliderImage/RightSliderImage";
+import "../../../components/leftImageRightSliderImage/leftImageRightSliderImage.css";
 
 const BrandingAdvertising = () => {
   return (
     <>
       <div>BrandingAdvertising</div>
+
+      {/* Left right image slider  start */}
+
+      <div className="leftImageRightSliderImage">
+        <div className="leftImageRightSliderImage_container">
+          <div className="leftImageRightSliderImage_wrapper">
+            <LeftImage image={leftImage} />
+            <div className="leftImageRightSliderImage_right">
+              <div className="leftImageRightSliderImage_right_top">
+                <RightSlider
+                  title1={"Brand Identity"}
+                  title2={"Brand Identity"}
+                  text1={
+                    "Creating almost a personification of Jolie, this beautiful wordmark feels feminine and clean. The varied weights and character angles of the custom wordmark combine to create a strong and timeless wordmark."
+                  }
+                  text2={
+                    "Creating almost a personification of Jolie, this beautiful wordmark feels feminine and clean. The varied weights and character angles of the custom wordmark combine to create a strong and timeless wordmark."
+                  }
+                />
+                <RightSliderImage image={rightTopImage} />
+              </div>
+              <div className="leftImageRightSliderImage_right_bottom">
+                <RightSliderImage image={rightBottomImage} />
+                <RightSlider
+                  title1={"Brand Messaging"}
+                  title2={"Brand Messaging"}
+                  text1={
+                    "The particular challenge of the brand voice definition was to communicate in a way that peaked the interest of the viewer and encouraged them to pay attention to the impact their water had on their wellness and beauty routines."
+                  }
+                  text2={
+                    "The particular challenge of the brand voice definition was to communicate in a way that peaked the interest of the viewer and encouraged them to pay attention to the impact their water had on their wellness and beauty routines."
+                  }
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      {/* Left right image slider  end */}
+
       <RelatedServices />
     </>
   );
