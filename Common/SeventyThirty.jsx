d0ec9cc706d41af4a3b19b2f10b2a1072c173759
utkarshyadav11 <utkarshyadav6745@gmail.com>
2023-10-12 17:07:53 +0530

    new fixes in inner pages ---u

--- Common/SeventyThirty.jsx
+++ Common/SeventyThirty.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import "./seventythirty.css"
 const SeventyThirty = (props) => {
   return (
+      <div className='common_container'>
+      
       <div className='seventy_thirty'>
       <div className='seventy_thirty_one'>
       <img className='seventy_img' src={props.seventy_img} alt="" />
       </div>
       <div className='seventy_thirty_two'>
       <img className='seventy_img2' src={props.thirty_img} alt="" />
+      </div>
       </div>
       </div>
   )
--- Common/innerCommonButton.jsx
+++ Common/innerCommonButton.jsx
@@ -3,10 +3,12 @@ import innerCommon from "./../src/images/innerbuttonimage.png"
 import "./innerbutton.css"
 const innerCommonButton = () => {
   return (
+    <div className='common_container'>
     <div style={{marginTop:"2rem"}}>
     <div className='common_button_outer'>
     <img src={innerCommon} alt="" />
     <a href='/contact-us'  style={{fontSize:"25px",position:"absolute",top:"24%",left:"40px",transfrom:"translateY(50%)"}}>Contact Now -</a>
+    </div>
     </div>
     </div>
   )
